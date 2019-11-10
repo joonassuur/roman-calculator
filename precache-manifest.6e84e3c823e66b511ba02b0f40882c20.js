a7da942b4d14b5ab4e1cfc5ca6c7a7f4cb143d2d
Joonas <joonas.suurvali@gmail.com>
2019-11-10 21:24:05 +0100

    Updates

---
+++ precache-manifest.6e84e3c823e66b511ba02b0f40882c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a7cb24fc4b1299b82589b85039caff",
+    "url": "/roman-calculator/index.html"
+  },
+  {
+    "revision": "803143a7d2e0cdd1b7d8",
+    "url": "/roman-calculator/static/css/main.fef139e3.chunk.css"
+  },
+  {
+    "revision": "2f81047f82f97b55f867",
+    "url": "/roman-calculator/static/js/2.312d7590.chunk.js"
+  },
+  {
+    "revision": "803143a7d2e0cdd1b7d8",
+    "url": "/roman-calculator/static/js/main.f3734d4c.chunk.js"
+  },
+  {
+    "revision": "a0117f5d4a5128632733",
+    "url": "/roman-calculator/static/js/runtime-main.331c61a5.js"
+  }
+]);
